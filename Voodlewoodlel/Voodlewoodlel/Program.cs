a4405afcc6ecbee3fb2ea3ba8afe74e2fb78ace8
Jiashu Yang <naxx09@gmail.com>
2017-03-28 14:31:40 -0400

    init

---
+++ Voodlewoodlel/Voodlewoodlel/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Voodlewoodlel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your friend's name: ");
+            string name = Console.ReadLine();
+
+            foreach(char c in name)
+            {
+                // if vowel is lower letter
+                if (c.Equals('a') ||
+                    c.Equals('e') ||
+                    c.Equals('i') ||
+                    c.Equals('o') ||
+                    c.Equals('u'))
+                {
+                    Console.Write("oodle");
+                }
+                // if vowel is upper letter
+                else if (c.Equals('A') ||
+                        c.Equals('E') ||
+                        c.Equals('I') ||
+                        c.Equals('O') ||
+                        c.Equals('U'))
+                {
+                    Console.Write("Oodle");
+                }
+                // if there is no vowel
+                else
+                {
+                    Console.Write(c);
+                }
+            }
+
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+        }
+    }
+}
